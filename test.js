64db26f0542a2dedc08bfd4eff1cb5a7c9bf68da
Phisher98 <mandiktest2@gmail.com>
2025-12-31 16:30:04 +0530

    mior fix

---
+++ test.js
@@ -0,0 +1,6 @@
+const { getStreams } = require('./providers/moviesdrive.js');
+
+getStreams('200875', 'tv',1,1).then(streams => {
+  console.log('Found', streams.length, 'streams');
+  streams.forEach(stream => console.log(`${stream.name}: ${stream.quality}`));
+}).catch(console.error);
