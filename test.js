6f88d1c6bddabf30d0768d76224d600ac32bab7c
Phisher98 <mandiktest2@gmail.com>
2025-12-31 15:53:40 +0530

    fix

--- test.js
+++
@@ -1,6 +0,0 @@
-const { getStreams } = require('./providers/moviesdrive.js');
-
-getStreams('1381405', 'movie').then(streams => {
-  console.log('Found', streams.length, 'streams');
-  streams.forEach(stream => console.log(`${stream.name}: ${stream.quality} : ${stream.url}`));
-}).catch(console.error);
